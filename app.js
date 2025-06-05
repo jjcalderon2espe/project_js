b04a85cd158309337322b26c910a667260d5e4ab
Dalinda Molina <damolina23@espe.edu.ec>
2025-06-05 18:11:27 -0500

    Paso 6 completado por Dalinda Molina <damolina23@espe.edu.ec>

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 alert("Abre la consola (F12) para usar la aplicación.\nLuego escribe iniciarPrograma(); para comenzar.");
 
 let usuarios = [];
+
+function mostrarMenu() {
+  console.log("=== Menú de Gestión de Usuarios y Tareas ===");
+  console.log("1. Ver usuarios existentes");
+  console.log("2. Agregar nuevo usuario");
+  console.log("3. Ver tareas de un usuario");
+  console.log("4. Agregar nueva tarea a un usuario");
+  console.log("5. Marcar tarea como completada");
+  console.log("6. Eliminar una tarea");
+  console.log("7. Salir del programa");
+}
